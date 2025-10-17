02878709cfdeee03dee7acb4cf4066d28f583169
zzimog <>
2025-10-17 17:40:25 +0200

    update

--- app/main.tsx
+++ app/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import AppContainer from './app/AppContainer.tsx';
+import AppContainer from '../app/AppContainer.tsx';
 import '@fontsource-variable/manrope/index.css';
 
 createRoot(document.getElementById('root')!).render(

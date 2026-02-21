7159bb36129505e6b57eec6d934d1ffa75a4e264
zzimog <null>
2026-02-21 01:58:09 +0100

    update

--- src/ui/components/Collapsible/Collapsible.tsx
+++ src/ui/components/Collapsible/Collapsible.tsx
@@ -56,6 +56,9 @@ export const Collapsible = (inProps: CollapsibleProps) => {
       data-open={open}
       {...props}
       className={cn(classes.root, className)}
+      style={{
+        animationFillMode: 'none',
+      }}
     />
   );
 };

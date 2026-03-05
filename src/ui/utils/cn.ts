1226bded8e831803f6d90b0c5306a1c058b0f2d5
grasim <simone.graziano@zucchetti.it>
2026-03-05 16:20:11 +0100

    refactor imports and export

--- src/ui/utils/cn.ts
+++ src/ui/utils/cn.ts
@@ -1,9 +1,6 @@
-import type { ClassValue } from 'clsx';
+import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import clsx from 'clsx';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export type { ClassValue };

e02982c4ad38130c9ed98173d51f159e255621ac
grasim <simone.graziano@zucchetti.it>
2026-03-23 10:40:44 +0100

    maybe mobile fix?

--- src/ui/headless/Popper/PopperContent.tsx
+++ src/ui/headless/Popper/PopperContent.tsx
@@ -1,4 +1,9 @@
-import { useEffect, useRef, type ComponentPropsWithRef } from 'react';
+import {
+  useLayoutEffect,
+  useRef,
+  useState,
+  type ComponentPropsWithRef,
+} from 'react';
 import { createPortal } from 'react-dom';
 import { Presence } from '@ui/headless';
 import { useMergedRefs } from '@ui/hooks';
@@ -9,63 +14,72 @@ const DISPLAY_NAME = 'PopperContent';
 
 type BaseProps = ComponentPropsWithRef<typeof Presence>;
 type PopperContentProps = BaseProps & {
-  avoidCollisions?: boolean;
   distance?: number;
   padding?: number;
   side?: 'top' | 'bottom' | 'left' | 'right';
   align?: 'start' | 'center' | 'end';
+  avoidCollisions?: boolean;
 };
 
 export const PopperContent = (inProps: PopperContentProps) => {
   const {
     ref: refProp,
-    avoidCollisions = false,
+    present,
     distance,
     padding,
     side,
     align,
+    avoidCollisions,
     style,
     ...props
   } = inProps;
 
   const { anchor } = Popper.useContext(DISPLAY_NAME);
 
+  const [isPlaced, setIsPlaced] = useState(false);
+
   const ref = useRef<HTMLElement>(null);
   const mergedRefs = useMergedRefs(refProp, ref);
 
-  useEffect(() => {
-    function handleResize() {
-      const content = ref.current;
-      if (anchor && content) {
-        placeContent({
-          anchor,
-          content,
-          distance,
-          padding,
-          side,
-          align,
-          avoidCollisions,
-        });
-      }
-    }
+  useLayoutEffect(() => {
+    if (present) {
+      function handleResize() {
+        const content = ref.current;
+        if (anchor && content) {
+          placeContent({
+            anchor,
+            content,
+            distance,
+            padding,
+            side,
+            align,
+            avoidCollisions,
+          });
+        }
 
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    window.addEventListener('scroll', handleResize, true);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-      window.removeEventListener('scroll', handleResize);
-    };
-  }, [anchor, distance, padding, side, align, avoidCollisions]);
+        setIsPlaced(true);
+      }
+
+      handleResize();
+      window.addEventListener('resize', handleResize);
+      window.addEventListener('scroll', handleResize, true);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+        window.removeEventListener('scroll', handleResize);
+      };
+    }
+  }, [present, anchor, distance, padding, side, align, avoidCollisions]);
 
   return createPortal(
     <Presence
       ref={mergedRefs}
+      present={present}
       {...props}
       style={{
         position: 'fixed',
         top: 0,
         left: 0,
+        animationName: isPlaced ? undefined : 'none',
         ...style,
       }}
     />,
